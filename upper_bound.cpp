2b251b2c5e92cbc59eef55b0509006004f35b9d8
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-04-25 17:21:18 +0800

    lower_bound | upper_bound

---
+++ upper_bound.cpp
@@ -0,0 +1,11 @@
+#include "STL_Hearders.h"
+#include <iostream>
+
+vector<int> vec = {1, 2, 2, 3};
+
+int main() {
+    int a = upper_bound(vec.begin(), vec.end(), 2) - vec.begin();
+    cout << a << endl;
+
+    return 0;
+}
